894bd1c9682f565ed197cd1fcc8da07e24689738
David Houdek <houdeda2@fit.cvut.cz>
2023-10-27 21:38:14 +0200

    Calculator: rework calculator for interactive use

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "Calculator.h"
 
 #define GREEN "\033[1;32m"
@@ -15,56 +17,95 @@ void findAndReplaceAll(std::string & expression, char toReplace, std::string rep
     }
 }
 
+double calculate(std::string expression, double savedRes) {
+    double res;
 
-int main() {
-    std::cout << PURPLE "Options:\n"
-                 RED "sin: " GREEN "sin previous result\n"
-                 RED "cos: " GREEN "cos previous result\n"
-                 RED "e:" GREEN " exit\n\n"
-                 PURPLE "You can also use 'X' as placeholder for previous answer, however it will be rounded to nearest whole number\n"
-                 RED "Permitted operators: " GREEN "+ - * / ^ ( )" RESET << std::endl;
+    findAndReplaceAll(expression, 'X', std::to_string( static_cast<int>(round(savedRes)) ) );
 
-    std::string expression;
-    double res = 0;
 
-    while(expression != "e"){
-        std::cout << PURPLE "Enter expression: " RESET BLUE;
-        getline(std::cin, expression);
-        std::cout << RESET << std::flush;
-
-        if(expression == "sin") {
-            std::cout << GREEN "Calculating: " BLUE "sin("  + std::to_string(res) + ')' + RESET << std::endl;
-            res = sin(res);
-        }
-        else if(expression == "cos") {
-            std::cout << GREEN "Calculating: " BLUE "cos("  + std::to_string(res) + ')' + RESET << std::endl;
-            res = cos(res);
-        }
-        else if(expression == "e") {
-            continue;
-        }
-        else {
-
-            findAndReplaceAll(expression, 'X', std::to_string( static_cast<int>(round(res)) ) );
-
-            std::cout << GREEN "Calculating: " BLUE + expression + RESET << std::endl;
-
-            try {
-                res = Calculator::calculate(expression);
-            } catch (const std::exception& e) {
-                std::cout << RED "Error: " << e.what() << RESET << std::endl;
-                return 1;
-            }
-        }
-
-        std::cout << GREEN "Result: " BLUE << res << RESET << std::endl;
+    try {
+        res = Calculator::calculate(expression);
+    } catch (const std::exception& e) {
+        std::cout << RED "Error: " << e.what() << RESET << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
+    return res;
+}
 
 
+int main() {
 
+    std::string expression;
+    char inC;
+    double res = 0, savedRes = 0;
+    bool saved = false;
+    int lPar = 0, pPar = 0;
 
+    while (true){
+        system("clear");
+        std::cout << PURPLE "Options:\n"
+                 RED "SIN: " GREEN "sin saved result\n"
+                 RED "COS: " GREEN "cos saved result\n"
+                 RED "s:" GREEN " save result\n"
+                 RED "e:" GREEN " erase last character\n"
+                 RED "q:" GREEN " quit\n\n"
+                 PURPLE "You can also use 'X' as placeholder for saved answer, however it will be rounded to nearest whole number\n"
+                 RED "Permitted operators: " GREEN "+ - * / ^ ( )" RESET << std::endl;
+        if(saved) {
+            std::cout << PURPLE "Saved result: " BLUE << savedRes << RESET << std::endl;
+        }
+        std::cout << GREEN "Result: " BLUE << res << RESET << std::endl;
+        std::cout << GREEN << "Expression: " << PURPLE << expression << std::flush;
+        system("/bin/stty raw");
+        std::cin >> inC;
+        system("/bin/stty cooked");
+        std::cout << RESET << std::flush;
 
+        switch (inC) {
+            case 'q':
+                return 0;
+            case 's':
+                saved = true;
+                savedRes = res;
+                break;
+            case 'e':
+                if( !expression.empty() )
+                expression.pop_back();
+                break;
+            default:
 
-    return 0;
+                expression += inC;
+
+                if(expression == "SIN") {
+                    res = sin(savedRes);
+                    break;
+                }
+                else if(expression == "COS") {
+                    res = cos(savedRes);
+                    break;
+                }
+
+                lPar = pPar = 0;
+
+                bool swBreak = false;
+                for(auto c : expression) {
+                    if(c != 'X' && !std::isdigit(c) && c != '+'
+                    && c != '-' && c != '*' && c != '/' && c != '^' && c != '(' && c != ')' ) {
+                        swBreak = true;
+                        break;
+                    }
+                    if(c == '(')
+                        ++lPar;
+                    else if (c == ')')
+                        ++pPar;
+                }
+                if(swBreak || ( ( !std::isdigit(inC) && inC != ')' && inC != 'X' ) || lPar != pPar )) {
+                    break;
+                }
+
+                res = calculate(expression, savedRes);
+                break;
+        }
+    }
 }
