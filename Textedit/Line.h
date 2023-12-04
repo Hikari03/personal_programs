36e790853b96bdbdc0db71bedb54d56b2bc6af3a
David Houdek <houdeda2@fit.cvut.cz>
2023-12-04 17:56:56 +0100

    Textedit: Create documentation

--- Textedit/Line.h
+++ Textedit/Line.h
@@ -12,21 +12,71 @@ public:
 
     explicit Line(std::string);
 
+    /**
+     * @brief Insert a character at the current cursor position
+     * @param c
+     */
     void insert(char c);
+
+    /**
+     * @brief Remove the character before the cursor
+     */
     void remove();
+
+    /**
+     * @brief Clears the whole line
+     */
     void clear();
+
+    /**
+     * @brief Move the cursor left
+     */
     void moveLeft();
+
+    /**
+     * @brief Move the cursor right
+     */
     void moveRight();
+
+    /**
+     * @brief Get the current cursor position
+     * @return The current cursor position
+     */
     unsigned int getCurrentCharacterIndex();
+
+    /**
+     * @brief Set the current cursor position
+     * @param index The new cursor position
+     */
     void setCurrentCharacterIndex(unsigned int index);
 
+    /**
+     * @brief Print the line
+     * @param selected Whether the line is selected or not
+     */
     void print(bool selected = false);
 
+    /**
+     * @brief Concatenate two lines
+     * @param line
+     */
     void insertLine(Line &line);
+
+    /**
+     * @brief Get the line's characters
+     * @return The line's characters
+     */
     const std::string & exportLine();
 
+    /**
+     * @brief Get the characters after the current selection
+     * @return The characters after the current selection
+     */
     std::string charsAfterCurrentSelection();
 
+    /**
+     * @brief Remove the characters after the current selection
+     */
     void removeAfterCurrentSelection();
 
 private:
