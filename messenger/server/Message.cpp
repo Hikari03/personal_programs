adad2fc8f8c5adc3a36823eee5e31dd12d8c9dec
David Houdek <david.houdek11@gmail.com>
2024-03-26 14:43:04 +0100

    messenger: remove in favor of separated repo

--- messenger/server/Message.cpp
+++
@@ -1,7 +0,0 @@
-#include "Message.h"
-
-#include <utility>
-
-Message::Message(std::string username, std::string message,
-                 const std::chrono::time_point<std::chrono::system_clock> &time):
-        _username(std::move(username)), _message(std::move(message)), _time(time) {}
--- messenger/server/cleaner.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <list>
-#include "Client.h"
-
-void cleaner(std::list<Client> & clients, std::vector<std::thread> & clientRunners, const bool & turnOff, std::mutex & clientsMutex);
